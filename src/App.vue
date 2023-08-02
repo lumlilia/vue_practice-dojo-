47eb06edac95b134374362afe29cf186eb91581e
lumlilia <136717338+lumlilia@users.noreply.github.com>
2023-08-02 23:12:38 +0900

    2023/08/01の応用(WIP)
    今後やること
    ・色を変える機能をみんなに付ける
    ・目の色も変えられるようにしたい

--- src/App.vue
+++ src/App.vue
@@ -1,101 +1,28 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BlockNekoChan from './components/BlockNekoChan.vue'
 
 export default{
   components:{
-    HelloWorld,
+    BlockNekoChan,
   },
 
   data(){
     return{
-      gridNums: [
-        1, 2, 2, 2, 2, 3, 1, 
-        3, 3, 2, 1, 3, 1, 1, 
-        2, 3, 3, 3, 1, 2, 3, 
-        3, 2, 1, 3, 3, 2, 1, 
-        2, 1, 3, 2, 3, 1, 3, 
-        1, 2, 1, 2, 2, 2, 1, 
-        3, 1, 1, 2, 1, 3, 0
-      ],
-
-      gridColors: ["#ff9090", "#90ff90", "#9090ff"],
-
-      buttons: ["↑", "←", "↓", "→"],
-      charaX: 1,
-      charaY: 1,
-
-      move: 1,
+      col: 'red',
     }
   },
 
   methods:{
-    charaMove(n){
-      const mul = -1 + (Math.floor(n / 2) * 2)
-      const add = this.move * mul
-
-      if(n % 2){
-        if(this.charaX + add > 0 && this.charaX + add < 8){
-          this.charaX += add
-        }
-      }
-      else{
-        if(this.charaY + add > 0 && this.charaY + add < 8){
-          this.charaY += add
-        }
-      }
-
-      this.move = this.gridNums[(this.charaY - 1) * 7 + this.charaX - 1]
-    }
   }
 }
 </script>
 
 <template>
-  <header v-if="move == 0" id="goal">
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <div id="pazzle_box">
-      <div v-for="(num, index) in gridNums" class="grids" :style="{
-        backgroundColor: gridColors[num - 1],
-        gridColumn: index % 7 + 1,
-        gridRow: Math.floor(index / 7) + 1
-      }"></div>
-
-      <div id="chara" :style="{
-        gridColumn: charaX,
-        gridRow: charaY
-        }">●</div>
-
-    </div>
-
-    <div id="control_box">
-      <div id="sample_box">
-        <div v-for="(color, index) in gridColors" class="sample_item">
-          <div class="sample_color" :style="{
-            backgroundColor: color,
-            gridColumn: index + 1,
-          }"></div>
-          <p class="sample_num" :style="{
-            gridColumn: index + 1,
-          }">{{index + 1}}</p>
-        </div>
-      </div>
-
-      <div id="button_box">
-        <button v-for="(label, index) in buttons" @click="charaMove(index)" :style="{
-          gridColumn: ((index % 2) ? index : 2),
-          gridRow: ((index < 3) ? index + 1 : index - 1)
-        }">{{label}}</button>
-
-        <p id="move_view">{{move}}</p>
-      </div>
-    </div>
-
+    <button @click="col = 'yellow'">yellow</button>
+    <BlockNekoChan :bgcolor="col" />
+    <BlockNekoChan bgcolor="lime" />
+    <BlockNekoChan bgcolor="blue" />
   </main>
 </template>
 
@@ -126,136 +53,4 @@ main{
   margin: 0 auto 2rem;
 }
 
-#pazzle_box, #button_box{
-  display: grid;
-  justify-items: center;
-  align-items: center;
-}
-
-#pazzle_box{
-  grid-template-columns: repeat(7, 1fr);
-  width: 280px;
-  height: 280px;
-  border: 2px #303030 solid;
-  box-sizing: content-box;
-}
-
-.grids{
-  border: 1px #303030 solid;
-  width: 40px;
-  height: 40px;
-  box-sizing: border-box;
-}
-
-#chara{
-  font-size: 24px;
-}
-
-#control_box{
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-}
-
-#sample_box{
-  display: flex;
-  flex-direction: row;
-  justify-content: space-evenly;
-}
-
-.sample_item{
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-}
-
-.sample_color{
-  height: 10px;
-}
-
-.sample_num{
-  text-align: center;
-  font-size: 20px;
-}
-
-#button_box{
-  grid-template-columns: repeat(3, 1fr);
-  width: 120px;
-  height: 120px;
-  margin: 8px auto 0 auto;
-}
-
-#button_box button, #move_view{
-  width: 40px;
-  height: 40px;
-  text-align: center;
-  font-size: 24px;
-  box-sizing: border-box;
-}
-
-#move_view{
-  grid-column: 2;
-  grid-row: 2;
-}
-
-#goal{
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 100;
-  background-color: rgba(255, 255, 255, 0.8);
-}
-
-@media (orientation: landscape){
-  main{
-    flex-direction: row;
-    width: 280px;
-  }
-  
-  #sample_box{
-    width: 150px;
-  }
-
-  .sample_item{
-    width: 40px;
-  }
-}
-
-@media (orientation: portrait){
-  main{
-    flex-direction: column;
-  }
-
-  #control_box{
-    margin-top: 15px;
-  }
-  
-  #sample_box{
-    width: 280px;
-  }
-
-  .sample_item{
-    width: 80px;
-  }
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/BlockNekoChan.vue
@@ -0,0 +1,186 @@
+<template>
+<div id="chara">
+  <div id="head_out">
+    <div id="ear1" class="ear" :style="{borderBottomColor: bgcolor}"></div>
+    <div id="ear2" class="ear" :style="{borderBottomColor: bgcolor}"></div>
+
+    <div id="head_in" :style="{backgroundColor: bgcolor}">
+      <div id="face">
+        <div id="eye1" class="eye"></div>
+        <div id="mouse"></div>
+        <div id="eye2" class="eye"></div>
+      </div>
+    </div>
+  </div>
+
+  <div id="body_out">
+    <div id="body_in" :style="{backgroundColor: bgcolor}"></div>
+    <div id="arm1" class="arm" :style="{backgroundColor: bgcolor}"></div>
+    <div id="arm2" class="arm" :style="{backgroundColor: bgcolor}"></div>
+    <div id="leg1" class="leg" :style="{backgroundColor: bgcolor}"></div>
+    <div id="leg2" class="leg" :style="{backgroundColor: bgcolor}"></div>
+  </div>
+</div>
+</template>
+
+<script>
+export default{
+  props: ["bgcolor"],
+
+  data(){
+    return{
+    }
+  },
+
+  methods:{
+  },
+}
+</script>
+
+<style scoped>
+#head_out,
+.ear,
+#face,
+#body_out{
+  background-color: unset !important;
+}
+
+
+#chara{
+  position: relative;
+  width: fit-content;
+  height: fit-content;
+}
+
+#head_out{
+  position: relative;
+  width: 60px;
+  height: 50px;
+  margin: 0 10px;
+  transform-origin: center bottom;
+  animation: 5s head linear infinite alternate;
+}
+
+.ear{
+  position: absolute;
+  width: 0;
+  height: 0;
+  border-top: none;
+  border-left: 8px transparent solid;
+  border-bottom: 15px solid;
+  border-right: 8px transparent solid;
+}
+#ear2{
+  right: 0;
+}
+
+#head_in{
+  position: absolute;
+  top: 15px;
+  width: 60px;
+  height: 45px;
+}
+
+#face{
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+#mouse{
+  position: absolute;
+  top: 50%;
+  left: calc(50% - 5px);
+  width: 10px;
+  height: 10px;
+  background-color: black !important;
+  clip-path: polygon(0% 0%, 10% 0%, 25% 70%, 50% 0%, 75% 70%, 90% 0%, 100% 0%, 75% 100%, 50% 30%, 25% 100%);
+}
+
+.eye{
+  position: absolute;
+  top: 45%;
+  width: 4px;
+  height: 10px;
+  color: blue;
+  background-color: black !important;
+  transform-origin: center;
+  animation: 7s eye linear infinite alternate;
+}
+#eye1{
+  left: 25%;
+}
+#eye2{
+  left: calc(75% - 4px);
+}
+
+#body_out{
+  position: relative;
+}
+
+#body_in{
+  position: absolute;
+  top: 0;
+  left: calc(50% - 20px);
+  width: 40px;
+  height: 40px;
+  transform-origin: center top;
+}
+
+.arm{
+  position: absolute;
+  top: 10px;
+  width: 30px;
+  height: 15px;
+  animation: 1s arm linear infinite alternate;
+}
+#arm1{
+  --rot: -30deg;
+  transform-origin: center right;
+}
+#arm2{
+  --rot: 30deg;
+  right: 0;
+  transform-origin: center left;
+}
+
+.leg{
+  position: absolute;
+  top: 35px;
+  width: 15px;
+  height: 25px;
+  margin: 0 20px;
+}
+#leg2{
+  right: 0;
+}
+
+
+@keyframes eye{
+  0%, 24%, 30%, 96%, 100%{
+    transform: scaleY(1);
+  }
+  27%, 98%{
+    transform: scaleY(0);
+  }
+}
+
+@keyframes head{
+  0%, 80%{
+    transform: rotate(0);
+  }
+  90%, 100%{
+    transform: rotate(10deg);
+  }
+}
+
+@keyframes arm{
+  0%{
+    transform: rotate(var(--rot));
+  }
+  100%{
+    transform: rotate(0);
+  }
+}
+</style>
+
